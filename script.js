08fe1c44a27c5f8529b2b15495d169acc66316a3
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-30 18:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-async function getdata() {
-	let res = await fetch("https://jsonplaceholder.typicode.com/todos/1")
-	.then(data=> console.log(res.data))
+function getdata() {
+	let res = fetch("https://jsonplaceholder.typicode.com/todos/1")
+	.then(data => console.log(res.data))
 }
 
 getdata()
