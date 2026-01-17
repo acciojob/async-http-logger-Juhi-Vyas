c63bd4dcb33b864e3f75278dcda7b9a91a02f508
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-17 23:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let res = fetch("https://jsonplaceholder.typicode.com/todos/1")
-.then(response => response.json)
-.then(data => console.log(data))
+cy.visit("http://localhost:3000", {
+  onBeforeLoad(win) {
+    cy.stub(win.console, "log").as("consoleLog");
+  }
+});
 
+cy.get("@consoleLog").should("be.calledWith", {
+  userId: 1,
+  id: 1,
+  title: "delectus aut autem",
+  completed: false
+});
